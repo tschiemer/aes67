5401d5c5755fe9f445453346822e461014f72dc8
philip <me@filou.se>
2021-02-23 12:05:09 +0100

    added simple test functionality for sdp parser

--- src/utils/sdp-parse/sdp-parse.c
+++ src/utils/sdp-parse/sdp-parse.c
@@ -30,16 +30,28 @@ static char * argv0;
 
 static struct {
     bool print_debug;
+    bool test;
+    enum aes67_audio_encoding encoding;
+    int samplerate;
+    int nchannels;
 } opts;
+
+//static bool use_filter(){
+//    return (opts.encoding != aes67_audio_encoding_undefined || opts.samplerate != 0 || opts.nchannels != 0);
+//}
 
 static void help(FILE * fd)
 {
     fprintf( fd,
-             "Usage: %s [-hd]\n"
-             "Attempts to parse SDP incoming on STDIN (primarily useful to validate custom SDP files quickly).\n"
+             "Usage: %s ([-h?] | [-t [-brc]])\n"
+             "Attempts to parse SDP incoming on STDIN (primarily useful to validate SDP files quickly).\n"
              "Options:\n"
              "\t -h\t Prints this info\n"
              "\t -d\t Prints some debug info to STDERR\n"
+             "\t -t\t Test if the first (SDP) packet (and only the first) contains at least one valid stream; return 0 if valid, >0 otherwise\n"
+             "\t -b\t Filter by bitwidth/encoding (8/16/24/32, representing L8, L16, L24, L32 respectively)\n"
+             "\t -r\t Filter by sampling rate (frequency); ex. 48000\n"
+             "\t -c\t Filter by number of channels required\n"
              , argv0);
 }
 
@@ -48,12 +60,48 @@ int main(int argc, char * argv[])
 {
     argv0 = argv[0];
 
+    opts.print_debug = false;
+    opts.test = false;
+    opts.encoding = aes67_audio_encoding_undefined;
+    opts.samplerate = 0;
+    opts.nchannels = 0;
+
     int opt;
 
-    while ((opt = getopt(argc, argv, "h?d")) != -1) {
+    while ((opt = getopt(argc, argv, "h?dtb:r:c:")) != -1) {
         switch (opt) {
             case 'd':
                 opts.print_debug = true;
+                break;
+
+            case 't':
+                opts.test = true;
+                break;
+
+            case 'b':{
+                int e = atoi(optarg);
+                if (e == 8){
+                    opts.encoding = aes67_audio_encoding_L8;
+                } else if (e == 16){
+                    opts.encoding = aes67_audio_encoding_L16;
+                } else if (e == 24){
+                    opts.encoding = aes67_audio_encoding_L24;
+                } else if (e == 32){
+                    opts.encoding = aes67_audio_encoding_L32;
+                } else {
+                    fprintf(stderr, "ERROR: invalid bitwidth option %s\n", optarg);
+                    return EXIT_FAILURE;
+                }
+            }
+                break;
+
+
+            case 'r':
+                opts.samplerate = atoi(optarg);
+                break;
+
+            case 'c':
+                opts.nchannels = atoi(optarg);
                 break;
 
             case 'h':
@@ -66,6 +114,7 @@ int main(int argc, char * argv[])
 
     if ( optind < argc ){ // 1 < argc &&
         fprintf(stderr, "ERROR too many arguments\n");
+        help(stdout);
         return EXIT_FAILURE;
     }
 
@@ -90,147 +139,202 @@ int main(int argc, char * argv[])
             }
         } else if (r > 0){ // data read
 
-            if (memcmp("v=0", rbuf, 3) == 0){
+            struct aes67_sdp sdp;
 
-                struct aes67_sdp sdp;
+            r = aes67_sdp_fromstr(&sdp, rbuf, r, NULL);
 
+            if (opts.test){
 
-                r = aes67_sdp_fromstr(&sdp, rbuf, r, NULL);
+                if (r != AES67_SDP_OK){
+                    return r;
+                }
 
-                if (r == AES67_SDP_OK){
+                // if either no connections, streams, refclock, or encodings given, it is an invalid (unusable) SDP
+                if (sdp.connections.count == 0 || sdp.streams.count == 0 || sdp.refclks.count == 0 || sdp.encodings.count == 0){
+                    return EXIT_FAILURE;
+                }
 
-                    sdp.originator.username.data[sdp.originator.username.length] = '\0';
-                    sdp.originator.address.data[sdp.originator.address.length] = '\0';
-                    sdp.originator.session_id.data[sdp.originator.session_id.length] = '\0';
-                    sdp.originator.session_version.data[sdp.originator.session_version.length] = '\0';
+                for( int s = 0; s < sdp.streams.count; s++){
 
-                    printf("SESSION %s@%s %s.%s", sdp.originator.username.data, sdp.originator.address.data, sdp.originator.session_id.data, sdp.originator.session_version.data);
-
-                    if ((sdp.ptp_domain & AES67_SDP_PTPDOMAIN_SET) == AES67_SDP_PTPDOMAIN_SET) {
-                        printf(" ptp-domain=%d", sdp.ptp_domain & AES67_SDP_PTPDOMAIN_VALUE);
+                    // skip stream if no valid connection given (shouldn't be the case in a valid SDP packet)
+                    if (NULL == aes67_sdp_get_connection(&sdp, s)){
+                        continue;
                     }
+
+                    // skip if stream has no valid refclock
+                    if (0 == aes67_sdp_get_refclk_count(&sdp, s)){
+                        continue;
+                    }
+
+                    // skip if stream has no encoding
+                    if (0 == aes67_sdp_get_stream_encoding_count(&sdp, s)){
+                        continue;
+                    }
+
+                    struct aes67_sdp_attr_encoding * encoding = aes67_sdp_get_stream_encoding(&sdp, s, 0);
+
+                    if (opts.nchannels > 0 && opts.nchannels != encoding->nchannels){
+                        continue;
+                    }
+                    if (opts.encoding != aes67_audio_encoding_undefined && opts.encoding != encoding->encoding){
+                        continue;
+                    }
+                    if (opts.samplerate != 0 && opts.samplerate != encoding->samplerate){
+                        continue;
+                    }
+
+
+                    return EXIT_SUCCESS;
+                }
+
+                return EXIT_FAILURE;
+            } // no test
+
+
+            if (r == AES67_SDP_OK){
+
+                sdp.originator.username.data[sdp.originator.username.length] = '\0';
+                sdp.originator.address.data[sdp.originator.address.length] = '\0';
+                sdp.originator.session_id.data[sdp.originator.session_id.length] = '\0';
+                sdp.originator.session_version.data[sdp.originator.session_version.length] = '\0';
+
+                printf("SESSION %s@%s %s.%s", sdp.originator.username.data, sdp.originator.address.data, sdp.originator.session_id.data, sdp.originator.session_version.data);
+
+                if ((sdp.ptp_domain & AES67_SDP_PTPDOMAIN_SET) == AES67_SDP_PTPDOMAIN_SET) {
+                    printf(" ptp-domain=%d", sdp.ptp_domain & AES67_SDP_PTPDOMAIN_VALUE);
+                }
 
 #if 0 < AES67_SDP_MAXSESSIONNAME
 //                    if (sdp.name.length){
-                        sdp.name.data[sdp.name.length] = '\0';
-                        printf(" \"%s\"", sdp.name.data);
+                    sdp.name.data[sdp.name.length] = '\0';
+                    printf(" \"%s\"", sdp.name.data);
 //                    }
 #endif
 #if 0 < AES67_SDP_MAXSESSIONINFO
 //                    if (sdp.info.length){
-                        sdp.info.data[sdp.info.length] = '\0';
-                        printf(" \"%s\"", sdp.info.data);
+                    sdp.info.data[sdp.info.length] = '\0';
+                    printf(" \"%s\"", sdp.info.data);
 //                    }
 #endif
 #if 0 < AES67_SDP_MAXTOOL
 //                    if (sdp.tool.length){
-                        sdp.tool.data[sdp.tool.length] = '\0';
-                        printf(" tool=%s", sdp.tool.data);
+                    sdp.tool.data[sdp.tool.length] = '\0';
+                    printf(" tool=%s", sdp.tool.data);
 //                    }
+#endif
+
+                printf("\n");
+
+                for(int i = 0; i < sdp.streams.count; i++){
+                    struct aes67_sdp_stream * stream = &sdp.streams.data[i];
+
+                    printf(" stream#%d ", i);
+
+                    struct aes67_sdp_connection * con = aes67_sdp_get_connection(&sdp, i);
+
+                    con->address.data[con->address.length] = '\0';
+                    printf("%s:%d/%d IP%c ", con->address.data, stream->port, stream->nports, con->ipver == aes67_net_ipver_4 ? '4' : '6');
+
+                    enum aes67_sdp_attr_mode mode = aes67_sdp_get_mode(&sdp, i);
+
+                    if (mode == aes67_sdp_attr_mode_inactive){
+                        printf("mode=inactive ");
+                    } else if (mode == aes67_sdp_attr_mode_recvonly){
+                        printf("mode=recvonly ");
+                    } else if (mode == aes67_sdp_attr_mode_sendonly){
+                        printf("mode=sendonly ");
+                    } else if (mode == aes67_sdp_attr_mode_sendrecv){
+                        printf("mode=sendrecv ");
+                    } else {
+                        printf("mode= ");
+                    }
+
+                    struct aes67_sdp_attr_mediaclk * mediaclk = aes67_sdp_get_mediaclock(&sdp, i);
+                    if (mediaclk && mediaclk->set) {
+                        printf("clk-offset=%u", mediaclk->offset);
+                    }
+
+                    if ( (stream->ptime & AES67_SDP_PTIME_SET) == AES67_SDP_PTIME_SET){
+                        printf(" ptime=%d", stream->ptime & AES67_SDP_PTIME_VALUE);
+                    } else {
+                        printf(" ptime=0");
+                    }
+
+                    printf(" nenc=%d nptp=%d", stream->nencodings, aes67_sdp_get_refclk_count(&sdp, i));
+
+#if 0 < AES67_SDP_MAXPTIMECAPS
+                    printf( " nptime-cap=%d", stream->ptime_cap.count);
+#endif
+#if 0 < AES67_SDP_MAXSTREAMINFO
+                    stream->info.data[stream->info.length] = '\0';
+                    printf(" \"%s\"", stream->info.data);
 #endif
 
                     printf("\n");
 
-                    for(int i = 0; i < sdp.streams.count; i++){
-                        struct aes67_sdp_stream * stream = &sdp.streams.data[i];
+                    for (int e = 0; e < sdp.streams.data[i].nencodings; e++){
 
-                        printf(" stream#%d ", i);
+                        printf("  enc#%d.%d ", i, e);
 
-                        struct aes67_sdp_connection * con = aes67_sdp_get_connection(&sdp, i);
+                        struct aes67_sdp_attr_encoding * enc = aes67_sdp_get_stream_encoding(&sdp, i, e);
 
-                        con->address.data[con->address.length] = '\0';
-                        printf("%s:%d/%d IP%c ", con->address.data, stream->port, stream->nports, con->ipver == aes67_net_ipver_4 ? '4' : '6');
+                        printf("L%d ", (enc->encoding & AES67_AUDIO_ENC_SAMPLESIZE) * 8);
 
-                        enum aes67_sdp_attr_mode mode = aes67_sdp_get_mode(&sdp, i);
+                        printf("%d %d\n", enc->samplerate, enc->nchannels);
+                    }
 
-                        if (mode == aes67_sdp_attr_mode_inactive){
-                            printf("mode=inactive ");
-                        } else if (mode == aes67_sdp_attr_mode_recvonly){
-                            printf("mode=recvonly ");
-                        } else if (mode == aes67_sdp_attr_mode_sendonly){
-                            printf("mode=sendonly ");
-                        } else if (mode == aes67_sdp_attr_mode_sendrecv){
-                            printf("mode=sendrecv ");
-                        } else {
-                            printf("mode= ");
+                    int np = aes67_sdp_get_refclk_count(&sdp, i);
+                    for(int p = 0; p < np; p++){
+
+                        printf("  refclk#%d.%d ", i, p);
+
+                        struct aes67_sdp_attr_refclk * clk = aes67_sdp_get_refclk(&sdp, i, p);
+
+                        if (clk->type == aes67_sdp_refclktype_localmac){
+                            printf("localmac %02x-%02x-%02x-%02x-%02x-%02x\n", clk->data.localmac[0], clk->data.localmac[1], clk->data.localmac[2], clk->data.localmac[3], clk->data.localmac[4], clk->data.localmac[5]);
                         }
-
-                        struct aes67_sdp_attr_mediaclk * mediaclk = aes67_sdp_get_mediaclock(&sdp, i);
-                        if (mediaclk && mediaclk->set) {
-                            printf("clk-offset=%u", mediaclk->offset);
+                        else if (clk->type == aes67_sdp_refclktype_ptptraceable){
+                            printf("ptp-traceable\n");
                         }
+                        else if (clk->type == aes67_sdp_refclktype_ptpclock){
 
-                        if ( (stream->ptime & AES67_SDP_PTIME_SET) == AES67_SDP_PTIME_SET){
-                            printf(" ptime=%d", stream->ptime & AES67_SDP_PTIME_VALUE);
-                        } else {
-                            printf(" ptime=0");
-                        }
+                            printf("ptp-clock ");
 
-                        printf(" nenc=%d nptp=%d", stream->nencodings, aes67_sdp_get_ptp_count(&sdp, i));
-
-#if 0 < AES67_SDP_MAXPTIMECAPS
-                        printf( " nptime-cap=%d", stream->ptime_cap.count);
-#endif
-#if 0 < AES67_SDP_MAXSTREAMINFO
-                        stream->info.data[stream->info.length] = '\0';
-                        printf(" \"%s\"", stream->info.data);
-#endif
-
-                        printf("\n");
-
-                        for (int e = 0; e < sdp.streams.data[i].nencodings; e++){
-
-                            printf("  enc#%d.%d ", i, e);
-
-                            struct aes67_sdp_attr_encoding * enc = aes67_sdp_get_stream_encoding(&sdp, i, e);
-
-                            printf("L%d ", (enc->encoding & AES67_AUDIO_ENC_SAMPLESIZE) * 8);
-
-                            printf("%d %d\n", enc->samplerate, enc->nchannels);
-                        }
-
-                        int np = aes67_sdp_get_ptp_count(&sdp, i);
-                        for(int p = 0; p < np; p++){
-
-                            printf("  ptp#%d.%d ", i, p);
-
-                            struct aes67_ptp * ptp = &aes67_sdp_get_ptp(&sdp, i, p)->ptp;
-
-                            if (ptp->type == aes67_ptp_type_IEEE1588_2002){
+                            if (clk->data.ptp.type == aes67_ptp_type_IEEE1588_2002){
                                 printf("%s ", AES67_PTP_TYPE_STR_IEEE1588_2002);
-                            } else if (ptp->type == aes67_ptp_type_IEEE1588_2008){
+                            } else if (clk->data.ptp.type == aes67_ptp_type_IEEE1588_2008){
                                 printf("%s ", AES67_PTP_TYPE_STR_IEEE1588_2008);
-                            } else if (ptp->type == aes67_ptp_type_IEEE1588_2019){
+                            } else if (clk->data.ptp.type == aes67_ptp_type_IEEE1588_2019){
                                 printf("%s ", AES67_PTP_TYPE_STR_IEEE1588_2019);
-                            } else if (ptp->type == aes67_ptp_type_IEEE802AS_2011){
+                            } else if (clk->data.ptp.type == aes67_ptp_type_IEEE802AS_2011){
                                 printf("%s ", AES67_PTP_TYPE_STR_IEEE802AS_2011);
                             }
 
-                            printf("%02X-%02X-%02X-%02X-%02X-%02X-%02X-%02X", ptp->gmid.u8[0], ptp->gmid.u8[1], ptp->gmid.u8[2], ptp->gmid.u8[3], ptp->gmid.u8[4], ptp->gmid.u8[5], ptp->gmid.u8[6], ptp->gmid.u8[7]);
+                            printf("%02X-%02X-%02X-%02X-%02X-%02X-%02X-%02X", clk->data.ptp.gmid.u8[0], clk->data.ptp.gmid.u8[1], clk->data.ptp.gmid.u8[2], clk->data.ptp.gmid.u8[3], clk->data.ptp.gmid.u8[4], clk->data.ptp.gmid.u8[5], clk->data.ptp.gmid.u8[6], clk->data.ptp.gmid.u8[7]);
 
-                            if (ptp->type == aes67_ptp_type_IEEE1588_2008 || ptp->type == aes67_ptp_type_IEEE1588_2019){
-                                printf(" %d", ptp->domain);
+                            if (clk->data.ptp.type == aes67_ptp_type_IEEE1588_2008 || clk->data.ptp.type == aes67_ptp_type_IEEE1588_2019){
+                                printf(" %d", clk->data.ptp.domain);
                             }
 
                             printf("\n");
                         }
 
-#if 0 < AES67_SDP_MAXPTIMECAPS
-                        if ((stream->ptime_cap.cfg & AES67_SDP_CAP_SET) == AES67_SDP_CAP_ACTIVE){
-                            printf("  ptime-acfg#%d %d\n", i, stream->ptime_cap.cfg & AES67_SDP_CAP_VALUE);
-                        }
-                        if ((stream->ptime_cap.cfg & AES67_SDP_CAP_SET) == AES67_SDP_CAP_PROPOSED){
+                    }
 
-                            for (int j = 0; j < stream->ptime_cap.count; j++){
-                                printf("  ptime-pcap#%d.%d ptime=%d %s\n", i, stream->ptime_cap.data[j].cap, stream->ptime_cap.data[j].ptime, (stream->ptime_cap.cfg_a == stream->ptime_cap.data[j].cap) ? "proposed" : "");
-                            }
+#if 0 < AES67_SDP_MAXPTIMECAPS
+                    if ((stream->ptime_cap.cfg & AES67_SDP_CAP_SET) == AES67_SDP_CAP_ACTIVE){
+                        printf("  ptime-acfg#%d %d\n", i, stream->ptime_cap.cfg & AES67_SDP_CAP_VALUE);
+                    }
+                    if ((stream->ptime_cap.cfg & AES67_SDP_CAP_SET) == AES67_SDP_CAP_PROPOSED){
+
+                        for (int j = 0; j < stream->ptime_cap.count; j++){
+                            printf("  ptime-pcap#%d.%d ptime=%d %s\n", i, stream->ptime_cap.data[j].cap, stream->ptime_cap.data[j].ptime, (stream->ptime_cap.cfg_a == stream->ptime_cap.data[j].cap) ? "proposed" : "");
                         }
+                    }
 #endif // 0 < AES67_SDP_MAXPTIMECAPS
 
 //                        printf("\n");
-                    }
                 }
-
             } else if (memcmp("o=", rbuf, 2) == 0){
 
                 struct aes67_sdp_originator origin;
